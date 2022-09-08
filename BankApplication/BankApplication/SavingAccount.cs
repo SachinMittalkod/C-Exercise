96d867503b112c12126a6a6d6bd4d08c319545d5
Sachin Mittalkod <Sachin.Mittalkod@euromonitor.com>
2022-09-08 11:08:49 +0530

    Exercise

---
+++ BankApplication/BankApplication/SavingAccount.cs
@@ -0,0 +1,42 @@
+﻿using BankApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_
+{
+    public class SavingAccount : Account
+    {
+        double balance;
+
+        public override void Deposit(double amount)
+        {
+            Console.WriteLine("Your current balance is {0}", balance);
+            if (amount > 500)
+            {
+                balance = balance + amount;
+                Console.WriteLine("Thank you for deposite your total balance is {0}", balance);
+            }
+            else
+            {
+                Console.WriteLine("Deposite must be greater than 500");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if (balance < amount)
+            {
+                Console.WriteLine("you cannot withdraw amount due to insufficient balance");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine("your total balance is {0}", balance);
+            }
+        }
+
+        public double Balance { get { return balance; } }
+    }
+}
